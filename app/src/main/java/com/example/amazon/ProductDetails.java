bb93200fc31946fb561cc45f3d4965c197a9074a
Hardik <groverhardik18@gmail.com>
2020-09-09 14:22:02 +0530

    product full des

---
+++ app/src/main/java/com/example/amazon/ProductDetails.java
@@ -0,0 +1,13 @@
+package com.example.amazon;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+    }
+}
