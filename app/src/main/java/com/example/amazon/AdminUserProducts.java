eb8d0b2ab9ad13297bd42c610dff32bbbb537055
Hardik <groverhardik18@gmail.com>
2020-09-20 13:47:00 +0530

    d

---
+++ app/src/main/java/com/example/amazon/AdminUserProducts.java
@@ -0,0 +1,82 @@
+package com.example.amazon;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.amazon.Model.Cart;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AdminUserProducts extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private DatabaseReference mRef;
+    private FirebaseAuth mAuth;
+    private String currentUserId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_user_products);
+        mRef = FirebaseDatabase.getInstance().getReference().child("admin View").child(currentUserId).child("Products");
+        recyclerView = findViewById(R.id.r4);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerOptions<Cart>options = new FirebaseRecyclerOptions.Builder<Cart>()
+                .setQuery(mRef,Cart.class).build();
+
+        FirebaseRecyclerAdapter<Cart,AdminUserProducts.ViewHolder>adapter =
+                new FirebaseRecyclerAdapter<Cart, ViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ViewHolder viewHolder, int i, @NonNull Cart cart) {
+                        viewHolder.price.setText(cart.getProductPrice());
+                        viewHolder.productName.setText(cart.getProductName());
+                        viewHolder.quantity.setText(cart.getProductQuantity());
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_items_layout,parent,false);
+                       ViewHolder viewHolder = new ViewHolder(view);
+                       return viewHolder;
+
+                    }
+                };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView productName,quantity,price;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            productName = itemView.findViewById(R.id.cart_name);
+            quantity = itemView.findViewById(R.id.cart_quantity);
+            price = itemView.findViewById(R.id.cart_price);
+        }
+    }
+}
